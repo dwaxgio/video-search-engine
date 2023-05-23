7db812970d61652c4676442950e0c45297916fe9
edward <edward_gio@hotmail.com>
2023-05-23 18:42:07 -0500

    Elements updated, to retreive only videos from y

--- js/index.js
+++ js/index.js
@@ -1,86 +1,214 @@
-var cse;
+// console.log("**reading js file");
 
-function initialize() {
-  google.search.cse.element.getElement("search").execute("");
+// // window.google = google;
+
+// // Initialize the PSE search element.
+// var cse = new google.cse.SearchElement("cse-search");
+// // console.log(cse);
+
+// // Set the search engine ID.
+// cse.setEngineId("c181a49ecc3cc4afa");
+
+// // Set the search query.
+// cse.setQuery("");
+
+// // Set the search results limit.
+// cse.setResultsLimit(10);
+
+// // Set the search results filter.
+// cse.setFilter("site=youtube.com&cat=music");
+
+// // Set the search results sort order.
+// cse.setSort("viewcount:desc");
+
+// // Display the search results.
+// cse.render();
+
+// // Handle the click event on the search results.
+// cse.addEventListener("resultClick", function (event) {
+//   // Get the clicked result.
+//   var result = event.result;
+
+//   // Open the video link in a new tab.
+//   window.open(result.link, "_blank");
+
+//   // Create a preview overlay.
+//   var previewOverlay = document.createElement("div");
+//   previewOverlay.className = "gcse-search-result-preview";
+
+//   // Add the video thumbnail to the preview overlay.
+//   var thumbnail = document.createElement("img");
+//   thumbnail.src = result.thumbnail;
+//   previewOverlay.appendChild(thumbnail);
+
+//   // Add the video title to the preview overlay.
+//   var title = document.createElement("h2");
+//   title.textContent = result.title;
+//   previewOverlay.appendChild(title);
+
+//   // Add the video description to the preview overlay.
+//   var description = document.createElement("p");
+//   description.textContent = result.description;
+//   previewOverlay.appendChild(description);
+
+//   // Add the preview overlay to the document body.
+//   document.body.appendChild(previewOverlay);
+
+//   // Handle the click event on the close button.
+//   document
+//     .querySelector(".gcse-search-result-preview .close-button")
+//     .addEventListener("click", function () {
+//       // Hide the preview overlay.
+//       previewOverlay.style.display = "none";
+//     });
+// });
+
+// // Handle the click event on the prev button.
+// document
+//   .querySelector(".gcse-search-results .prev-button")
+//   .addEventListener("click", function () {
+//     // Get the current page number.
+//     var currentPage = parseInt(
+//       document.querySelector(".gcse-search-results .page-number").textContent
+//     );
+
+//     // If the current page number is not 1, then go to the previous page.
+//     if (currentPage > 1) {
+//       currentPage--;
+
+//       // Update the page number.
+//       document.querySelector(".gcse-search-results .page-number").textContent =
+//         currentPage;
+
+//       // Fetch the search results for the current page.
+//       cse.setPage(currentPage);
+//       cse.render();
+//     }
+//   });
+
+// // Handle the click event on the next button.
+// document
+//   .querySelector(".gcse-search-results .next-button")
+//   .addEventListener("click", function () {
+//     // Get the current page number.
+//     var currentPage = parseInt(
+//       document.querySelector(".gcse-search-results .page-number").textContent
+//     );
+
+//     // If the current page number is not the last page, then go to the next page.
+//     if (currentPage < cse.getPages()) {
+//       currentPage++;
+
+//       // Update the page number.
+//       document.querySelector(".gcse-search-results .page-number").textContent =
+//         currentPage;
+
+//       // Fetch the search results for the current page.
+//       cse.setPage(currentPage);
+//       cse.render();
+//     }
+//   });
+
+// const container = document.getElementById("container");
+// const overlay = document.querySelector(".gsc-results-wrapper-overlay.gsc-results-wrapper-visible");
+// container.appendChild(overlay);
+
+// // Get the button element
+// var button = document.getElementsByClassName('gsc-search-button gsc-search-button-v2');
+
+// // Define the function to be executed when the button is pressed
+// function handleButtonClick() {
+//   // Your code logic goes here
+//   console.log("Button pressed!");
+//   alert("Button pressed!");
+//   // Call your function or perform any actions you want
+// }
+
+// // Attach an event listener to the button
+// button.addEventListener('click', handleButtonClick);
+
+//
+
+// // Get all the elements with class gsc-webResult gsc-result
+// var elements = document.getElementsByClassName('gsc-webResult gsc-result');
+
+// // Loop through the elements in reverse order to safely remove them
+// for (var i = elements.length - 1; i >= 0; i--) {
+//   var element = elements[i];
+
+//   // Check if the element's structure and content match the specified criteria
+//   var isValid = (
+//     element.innerHTML.includes('Lost [Official Music Video] - <b>Linkin Park</b> - YouTube') &&
+//     element.innerHTML.includes('https://www.youtube.com/watch?v=7NK_JOkuSVY')
+//   );
+
+//   // If the element does not match the criteria, remove it
+//   if (!isValid) {
+//     element.parentNode.removeChild(element);
+//   }
+// }
+
+//
+// Function to check if the element exists
+function checkElementExistence() {
+  var overlayElement = document.querySelector(
+    ".gsc-results-wrapper-overlay.gsc-results-wrapper-visible"
+  );
+  // var overlayElement = document.querySelector('.gsc-expansionArea');
+
+  if (overlayElement) {
+    // Element exists, perform your desired action here
+    console.log("Element found!");
+    // You can trigger an alert or execute any other code *******
+
+    // Get all the div elements with the class "gsc-webResult gsc-result"
+    var elements = document.querySelectorAll("div.gsc-webResult.gsc-result");
+
+    // Iterate through each element
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+
+      // Check if the structure and inner HTML content match the specified pattern
+      var isValidElement =
+        element.innerHTML.includes(
+          'class="gs-title"><a class="gs-title" href="https://www.youtube.com/watch?'
+        ) &&
+        element.innerHTML.includes(
+          'class="gs-bidi-start-align gs-visibleUrl gs-visibleUrl-short" dir="ltr">www.youtube.com</div>'
+        ) &&
+        element.innerHTML.includes(
+          'class="gs-bidi-start-align gs-visibleUrl gs-visibleUrl-long" dir="ltr" style="word-break:break-all;">'
+        );
+
+      // Delete the element if it doesn't match the pattern
+      if (!isValidElement) {
+        element.parentNode.removeChild(element);
+      }
+    }
+  }
 }
 
-function onSearchComplete() {
-  var searchTerm = document.getElementById("search").value;
-  var results = cse.search(searchTerm, {
-    safe: "active",
-    filter: "1",
-    num: 10,
-    sort: "viewcount",
+// Function to be called when DOM changes
+function handleDOMChanges() {
+  // Check if the element exists initially
+  checkElementExistence();
+
+  // Use MutationObserver to listen for changes in the DOM
+  var observer = new MutationObserver(function (mutationsList) {
+    for (var mutation of mutationsList) {
+      if (mutation.type === "childList") {
+        // DOM has changed, check if the element exists
+        checkElementExistence();
+      }
+    }
   });
 
-  var resultsDiv = document.getElementById("results");
-  resultsDiv.innerHTML = "";
-
-  for (var i = 0; i < results.length; i++) {
-    var result = results[i];
-    var videoResult = document.createElement("div");
-    videoResult.className = "video-result";
-
-    var img = document.createElement("img");
-    img.src = result.image.src;
-    videoResult.appendChild(img);
-
-    var info = document.createElement("div");
-    info.className = "info";
-
-    var title = document.createElement("h4");
-    title.textContent = result.title;
-    info.appendChild(title);
-
-    var views = document.createElement("p");
-    views.textContent = "Views: " + result.viewCount;
-    info.appendChild(views);
-
-    videoResult.appendChild(info);
-
-    resultsDiv.appendChild(videoResult);
-
-    videoResult.onclick = (function (result) {
-      return function () {
-        openPreview(result.link);
-      };
-    })(result);
-  }
-
-//   cse.setSiteRestriction("www.youtube.com");
+  // Start observing the DOM changes
+  observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true,
+  });
 }
 
-function openPreview(link) {
-  var previewOverlay = document.getElementById("preview-overlay");
-  var visitButton = document.getElementById("visit-button");
-  var closeButton = document.getElementById("close-button");
-
-  visitButton.onclick = function () {
-    window.open(link, "_blank");
-    closePreview();
-  };
-
-  closeButton.onclick = function () {
-    closePreview();
-  };
-
-  previewOverlay.style.display = "block";
-}
-
-function closePreview() {
-  var previewOverlay = document.getElementById("preview-overlay");
-  previewOverlay.style.display = "none";
-}
-
-window.onload = function () {
-  cse = new google.search.CustomSearchControl("c181a49ecc3cc4afa");
-  cse.setResultSetSize(10);
-  cse.setSearchCompleteCallback(this, onSearchComplete);
-  cse.setSiteRestriction("www.youtube.com");
-  cse.draw("results");
-
-  var searchButton = document.getElementById("search-button");
-  searchButton.onclick = function () {
-    initialize();
-  };
-};
+// Call the function to start checking for the element existence
+handleDOMChanges();
